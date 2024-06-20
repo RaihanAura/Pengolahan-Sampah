f06d3553a8d1a4ffae3ac608d9138a358aa9859c
RaihanAura <raihanayob@gmail.com>
2024-06-20 11:58:45 +0700

    LaporanIndex

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\LaporanController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -15,3 +16,4 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 
 
 Route::resource('users',UserController::class);
+Route::resource('laporans',LaporanController::class);
